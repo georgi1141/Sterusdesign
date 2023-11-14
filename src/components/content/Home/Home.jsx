bdd99c85eadc052b6c8f9204f262d411139f495b
georgi1141 <g.zh.yanev@gmail.com>
2023-11-14 21:01:12 +0100

    progress on home homponent

--- src/components/content/Home/Home.jsx
+++ src/components/content/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import "./home.css"
+import {Link} from "react-router-dom"
+import sterus from "../../../assets/images/IMG_7965.jpg"
 
 function Home() {
   return (
-    <div>Home component</div>
-  )
-}
+    <div className="landing-container">
+      <section className="hero">
+        <div className="hero-content">
+          <h1 className="hero-title">Explore Unique Car Interiors</h1>
+          <p className="hero-subtitle">Discover and showcase stunning car interiors</p>
+          <Link to={"/login"}><a className="cta-button">Start Your Journey</a></Link>
+          
+        </div>
+      </section>
+
+
+
+    </div>
+  );
+};
 
 export default Home
