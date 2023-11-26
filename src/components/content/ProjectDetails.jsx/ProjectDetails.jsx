500ee0efb8031fffd91461d342db9e3205d6f64a
georgi1141 <g.zh.yanev@gmail.com>
2023-11-26 21:02:42 +0100

    style ProjectDetails component

--- src/components/content/ProjectDetails.jsx/ProjectDetails.jsx
+++ src/components/content/ProjectDetails.jsx/ProjectDetails.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getOne } from '../../../services/projectService'
-
-
+import styles from './projectDetails.module.css'
+import { useGlobalContext } from '../../globalContext/GlobalAppContext'
 
 
 function ProjectDetails() {
 
     const { projectId } = useParams('projectId')
+    const { user } = useGlobalContext()
 
     const ACTION_KEYS = {
         EDIT: 'EDIT',
@@ -23,6 +24,13 @@ function ProjectDetails() {
         switch (action.type) {
             case ACTION_KEYS.POPULATE:
                 return { ...state, project: action.payload.res }
+            case ACTION_KEYS.DELETE :
+                 console.log('delete clicked') 
+                  return {...state}
+            case ACTION_KEYS.EDIT:
+                 console.log('edit clicked')
+                 return {...state}
+            default: return state;
         }
     }
 
@@ -37,14 +45,30 @@ function ProjectDetails() {
             console.log(error)
         }
     }, [])
-
-
+    const isOwner = user?._id === state.project._ownerId
 
 
     return (
-        <div>
-            description : {state.project._id}
+        <div className={styles.cardContainer}>
+            <div className={styles.imageContainer}>
+                <div className={styles.imageContainer}>
+                    <h3>{state.project.projectName}</h3>
+                    <img src={state.project.imageUrl} alt="project-image" />
+                </div>
+                <div className={styles.textContainer}>
+                    <p>{state.project.description}</p>
+                    
+                </div>
+                {isOwner
+                    &&
+                    <div className={styles.buttons}>
+                        <button onClick={()=>{dispatch({type:ACTION_KEYS.EDIT})}}>Edit</button>
+                        <button onClick={()=>{dispatch({type:ACTION_KEYS.DELETE})}}>Delete</button>
+                    </div>
+                }
 
+            </div >
+            <div className={styles.comments}>Comments: TODO...</div>
         </div>
     )
 }
